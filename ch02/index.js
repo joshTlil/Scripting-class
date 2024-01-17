46c0f3c29831943a265e447a64997627df5d921b
JoshuaT <jttsat170@gmail.com>
2024-01-17 05:27:32 -0500

    Part 1 of hands on project

---
+++ ch02/index.js
@@ -0,0 +1,17 @@
+var far = document.querySelector("#fValue");
+var cel = document.querySelector("#cValue");
+document.onkeydown = function(event){
+    if(event.key === "Tab"){
+        if(far.value !== ""){
+      cel.value = calCelsius(far.value);
+        }else {
+            console.log("Nothing!");
+        }
+    }
+}
+
+function calCelsius(value){
+    console.log("This is value: " + value);
+return (value - 32) / 1.8;
+}
+
